95ed2c82f2039ec7118e057e5a4a660c7f3e90f4
aleebit <alee@madisoncollege.edu>
2021-12-09 11:43:35 -0600

    messing wit RxJS

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable, merge, pipe } from 'rxjs';
+import { repeat, map, tap} from 'rxjs/operators';
+
+interface SwapiPlanetDataWeCareAbout{
+  next: string;
+  results: {
+    name: string;
+  }[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(
+    private httpSvc: HttpClient
+  ) { }
+
+    loadPlanets = (): Observable<SwapiPlanetDataWeCareAbout> => {
+      const page1 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>('https://swapi.dev/api/planets');
+      const page2 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>('https://swapi.dev/api/planets/?page=2')
+      .pipe(
+        tap(x=> console.log(x))
+        //,repeat(3)
+        ,map(x => ({
+          next: x.next
+          , results: x.results.map(y => ({
+            name: y.name
+          }))
+        }))
+        ,tap(x => console.log(x))
+      )
+      ;
+      return merge(
+        page1
+        , page2
+      );
+    };
 }
